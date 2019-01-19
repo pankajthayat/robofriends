3055d4ff88aaa5a0ff9d135be0d3442076825f2c
pankaj thayat <pankajthayat@gmail.com>
2019-01-19 10:13:03 +0530

    experiment : try to render multiple element using array without any parent div

--- src/CardList.js
+++ src/CardList.js
@@ -4,10 +4,11 @@ export default ({robots})=>{
     const cardComponent=robots.map((user,i)=>{
         return(<Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />)
     })
-
+    const x=[<Card />,<Card/>]
+    const y=[1,2,3]
     return(
 cardComponent
-
+//x
     )
 
 }
